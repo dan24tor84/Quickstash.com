fa066b0bd866907b6aaade9a0b88c8b7decb0141
dan24tor84 <dan24tor84@gmail.com>
2025-07-04 15:28:40 -0700

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 
-import CustomerOrderForm from './pages/CustomerOrderForm';
-import AdminOrdersDashboard from './pages/AdminOrdersDashboard';
+import VendorSignup from './pages/VendorSignup';
 import VendorDashboard from './pages/VendorDashboard';
 import CourierDashboard from './pages/CourierDashboard';
+import AdminDashboard from './pages/AdminDashboard';
 import ProductMenu from './pages/ProductMenu';
+import CustomerOrderForm from './pages/CustomerOrderForm';
+import CheckoutPage from './pages/CheckoutPage'; // We'll create this next
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<CustomerOrderForm />} />
-        <Route path="/admin/orders" element={<AdminOrdersDashboard />} />
-        <Route path="/vendor/dashboard" element={<VendorDashboard />} />
-        <Route path="/courier/dashboard" element={<CourierDashboard />} />
-        <Route path="/menu" element={<ProductMenu />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProductMenu />} />
+          <Route path="/order" element={<CustomerOrderForm />} />
+          <Route path="/vendor/signup" element={<VendorSignup />} />
+          <Route path="/vendor/dashboard" element={<VendorDashboard />} />
+          <Route path="/courier/dashboard" element={<CourierDashboard />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
