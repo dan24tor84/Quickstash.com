1dbca5aeb4c288473b95601cba9432d0e48ebf70
dan24tor84 <dan24tor84@gmail.com>
2025-07-04 05:17:33 -0700

    Update index .js

--- index .js
+++ index .js
@@ -1,1 +1,11 @@
-// Frontend entry point
+// FILE: frontend/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
