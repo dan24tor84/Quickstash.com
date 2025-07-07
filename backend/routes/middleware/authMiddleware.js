2c017824a30ce0499a6056f5deca3d48afd2655c
Dan <dan24tor84@gmail.com>
2025-07-06 21:14:27 -0700

    Create authMiddleware.js

---
+++ backend/routes/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'dev_secret_key';
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Expecting "Bearer TOKEN"
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authenticateToken;
